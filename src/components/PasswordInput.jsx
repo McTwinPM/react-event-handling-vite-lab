d205638fdcddf15638dce08172c918511b850658
Michael McCormack <mcctwin@gmail.com>
2025-07-16 21:23:50 -0500

    4 tests passed, 2 failed

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,8 +1,24 @@
 // Code PasswordInput Component Here
+import React from 'react';
 
 function PasswordInput (){
+    const handlePassword = (event) => {
+        event.preventDefault();
+    }
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            console.log("Enter button pressed");
+        }
+    }
     return(
-        <></>
+        <>
+        <form onSubmit={handlePassword}>
+        <input type="password" placeholder="Enter your password"
+        onChange={(e)=> console.log("Entering password...")}
+        onKeyDown={handleKeyDown} />
+        </form>
+        
+        </>
     )
 }
 
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,9 +1,32 @@
 // Code SubmitButton Component Here
+import React from 'react';
+
 
 function SubmitButton (){
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+    const hoverEnter = () => {
+        console.log("Mouse Entering");
+    }
+    const hoverLeave = () => {
+        console.log("Mouse Exiting");
+    }
     return(
-        <></>
-    )
+        <>
+        <form 
+        onSubmit={handleSubmit}>
+        <button type="submit"> Submit Password</button>
+        <div 
+        onMouseEnter={() => hoverEnter}
+        onMouseLeave={() => hoverLeave}>
+            Hover over me!
+        </div>
+        </form>        
+        
+        </>
+        )
+    
 }
 
 export default SubmitButton;
